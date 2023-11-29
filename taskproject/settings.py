fa596070e7a3bd1ed2c75ee553dbd016359f55d2
aluk1234 <RevenueInMyLife@gmail.com>
2023-11-29 14:23:01 +0900

    Fucking commit 1

--- taskproject/settings.py
+++ taskproject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = 'home/ubuntu/static'
+STATIC_ROOT = '/home/ubuntu/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
